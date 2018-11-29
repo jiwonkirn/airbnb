f4e514950cd89be7a82e5402d9a1fc902ba800dd
kimyunje <stoneaxe002@gmail.com>
2018-11-29 19:46:15 +0900

    css: 숙소리스트 전체 레이아웃 완성

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import { ReactComponent as Star } from "../svg/star.svg";
+import style from "../css/List.module.scss";
+import style2 from "../css/Star.module.scss";
 
 export default class ListView extends Component {
   render() {
     const { rooms } = this.props;
     return (
-      <div>
-        <h1>전세계의 숙소</h1>
-        {rooms.map(room => (
-          <div className="roomInfo">
-            <p>{room.location}</p>
-            <p>{room.title}</p>
-          </div>
-        ))}
+      <div className={style.listWrapper}>
+        <h1 className={style.listTitle}>전세계의 숙소</h1>
+        <div className={style.roomInfoWrapper}>
+          {rooms.map(room => (
+            <div className={style.roomInfo}>
+              <div className={style.roomImg} />
+              <p className={style.roomLocation}>{room.location}</p>
+              <p className={style.roomTitle}>{room.title}</p>
+              <p className={style.roomPrice}>{room.price}</p>
+              <div className={style.starwrapper}>
+                <Star className={style2.star} />
+                <Star className={style2.star} />
+                <Star className={style2.star} />
+                <Star className={style2.star} />
+                <Star className={style2.star} />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
--- src/containers/List.js
+++ src/containers/List.js
@@ -8,15 +8,43 @@ export default class List extends Component {
       rooms: [
         {
           location: "개인실 바르셀로나",
-          title: "SUNSET CAVE HOUSE IN BARSELONA"
+          title: "SUNSET CAVE HOUSE IN BARSELONA",
+          price: 62770
         },
         {
           location: "개인실 제주시",
-          title: "SOMETIMES JEJU 201호"
+          title: "SOMETIMES JEJU 201호",
+          price: 62770
         },
         {
           location: "개인실 서울",
-          title: "Myeong Dong Portabl Wifi #1"
+          title: "Myeong Dong Portabl Wifi #1",
+          price: 62770
+        },
+        {
+          location: "아파트 전체 레체",
+          title: "In the historical center in Lecce",
+          price: 62770
+        },
+        {
+          location: "개인실 바르셀로나",
+          title: "SUNSET CAVE HOUSE IN BARSELONA",
+          price: 62770
+        },
+        {
+          location: "개인실 제주시",
+          title: "SOMETIMES JEJU 201호",
+          price: 62770
+        },
+        {
+          location: "개인실 서울",
+          title: "Myeong Dong Portabl Wifi #1",
+          price: 62770
+        },
+        {
+          location: "아파트 전체 레체",
+          title: "In the historical center in Lecce",
+          price: 62770
         }
       ]
     };
