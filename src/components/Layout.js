e426661f11471cf64f163bc282b156ba2b139cc2
Ham Choromi <choromi0804@gmail.com>
2018-12-01 02:37:59 +0900

    Header 1st commit Deployed Header in the Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import style from './Layout.scss';
 import { ReactComponent as Logo } from '../svg/logo.svg';
 
-export default class extends React.Component {
+export default class Layout extends Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -10,7 +11,18 @@ export default class extends React.Component {
   render() {
     return (
       <header className="header">
-        <Logo style={{ width: '40px' }} />
+        <Logo className="logo" style={{ width: '40px', height: '50px' }} />
+        <input
+          type="search"
+          className="search"
+          required
+          placeholder="홍콩에 가보는건 어떠세요?"
+        />
+        <nav className="navbar">
+          <p className="navbar_likelist">저장목록</p>
+          <p className="navbar_visited">여행</p>
+          <p className="navbar_helpdesk">도움말</p>
+        </nav>
       </header>
     );
   }
