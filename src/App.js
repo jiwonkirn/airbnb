bc5b1f3289e39c0e429ce44dc9c2e4aeadb5b3e3
Kimjiwon <jhd1925@gmail.com>
2018-11-30 17:44:00 +0900

    Page 네이밍 정리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout';
-import Home from './pages/Home';
-import List from './pages/List';
-import Detail from './pages/Detail';
+import HomePage from './pages/HomePage';
+import ListPage from './pages/ListPage';
+import DetailPage from './pages/DetailPage';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './css/module.scss';
 
@@ -14,9 +14,9 @@ class App extends Component {
         <main className="mainSection">
           <BrowserRouter>
             <Switch>
-              <Route path="/search-list" component={List} />
-              <Route path="/room-detail/:roomId" component={Detail} />
-              <Route exact path="/" component={Home} />
+              <Route path="/search-list" component={ListPage} />
+              <Route path="/room-detail/:roomId" component={DetailPage} />
+              <Route exact path="/" component={HomePage} />
             </Switch>
           </BrowserRouter>
         </main>
