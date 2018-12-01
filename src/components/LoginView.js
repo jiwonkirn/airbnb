a333cf0243d232a438d68daceabf2d08accaa5e3
kimyunje <stoneaxe002@gmail.com>
2018-12-01 16:19:40 +0900

    google 로그인 버튼 생성

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 import { withUser } from '../contexts/UserContext';
 
 class LoginView extends Component {
@@ -7,6 +8,10 @@ class LoginView extends Component {
     console.log(process.env.REACT_APP_FACEBOOKID);
     const { setProfile, appId } = this.props;
     console.log(appId);
+    const responseGoogle = response => {
+      console.log(response);
+    };
+    document.getElementById('googleButton');
     return (
       <div>
         <FacebookLogin
@@ -14,6 +19,12 @@ class LoginView extends Component {
           autoLoad={true}
           fields="name,email,picture"
           callback={setProfile}
+        />
+        <GoogleLogin
+          clientId="492203123541-mv2nrcvptponmmqbaka554a06p7qjilc.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
         />
       </div>
     );
