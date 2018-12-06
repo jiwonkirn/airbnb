8a0f29e028b32a873037e2e5e665246cefa65ed9
kimyunje <stoneaxe002@gmail.com>
2018-12-06 14:40:34 +0900

    유저 정보 불러오려다가 실패...

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -23,6 +23,14 @@ export default class UserProvider extends Component {
   // 컴포넌트가 마운트 되면 로그인 여부를 확인한다.
   async componentDidMount() {
     await this.refreshUser();
+    //서버에서 사용자의 id와 username정보를 받아와서 상태를 바꿔주는 코드
+    // const {data: email, first_name, last_name, user_id} = await api.get(`/api/user`)
+    //   this.setState({
+    //     email,
+    //     first_name,
+    //     last_name,
+    //     user_id,
+    //   });
   }
 
   // 페이스북 에서 응답받은 콜백을 통해 로그인, 회원가입 요청을 하는 메소드
@@ -75,15 +83,8 @@ export default class UserProvider extends Component {
       user_id,
     });
     localStorage.setItem('token', res2.data.token);
-
-    //TODO:서버에서 사용자의 id와 username정보를 받아와서 상태를 바꿔주는 코드
-    this.setState({
-      email,
-      first_name,
-      last_name,
-      user_id,
-    });
   }
+
   render() {
     return <Provider value={this.state}>{this.props.children}</Provider>;
   }
