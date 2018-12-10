680c24ffcdea0b0f0902ff9c3ebcaf42cb868240
kimyunje <stoneaxe002@gmail.com>
2018-12-10 17:27:52 +0900

    예약 페이지 레이아웃 완성

--- src/components/ReserveView.js
+++ src/components/ReserveView.js
@@ -5,6 +5,8 @@ import { ReactComponent as ArrowDown } from '../svg/arrowDown.svg';
 import { ReactComponent as Star } from '../svg/star.svg';
 import { ReactComponent as Guest } from '../svg/guest.svg';
 import { ReactComponent as Calender } from '../svg/calender.svg';
+import { ReactComponent as Arrow } from '../svg/arrow.svg';
+import { ReactComponent as QuestionMark } from '../svg/questionMark.svg';
 import withCommonLoading from '../hoc/CommonLoading';
 import { withSearch } from '../contexts/SearchContext';
 class ReserveView extends Component {
@@ -44,6 +46,7 @@ class ReserveView extends Component {
       children,
       checkIn,
       checkOut,
+      price,
     } = this.props;
     console.log(room_photos);
     return (
@@ -152,7 +155,7 @@ class ReserveView extends Component {
           <div className={style.infoBox}>
             <div className={style.mainInfo}>
               <div className={style.textInfo}>
-                <h2>{room_name}</h2>
+                <h2 className={style.roomType}>{room_name}</h2>
                 <p>
                   {public_address}의 {room_type}
                 </p>
@@ -173,22 +176,52 @@ class ReserveView extends Component {
               </div>
             </div>
             <hr className={style.devider} />
-            <ul className={style.guestDateContainer}>
-              <li className={style.guestInfo}>
-                {' '}
-                <div className={style.iconWrapper}>
-                  <Guest className={style.guest} />
-                </div>{' '}
-                <p>게스트 {adult + children}명</p>
-              </li>
-              <li>
-                <div className={style.iconWrapper}>
-                  <Calender className={style.calender} />
-                </div>
-                <p>{checkIn}</p>
-                <p>{checkOut}</p>
-              </li>
-            </ul>
+            <div className={style.subWrapper}>
+              <ul className={style.guestDateContainer}>
+                <li className={style.guestInfo}>
+                  {' '}
+                  <div className={style.iconWrapper}>
+                    <Guest className={style.guest} />
+                  </div>{' '}
+                  <p>게스트 {adult + children}명</p>
+                </li>
+                <li className={style.dateInfo}>
+                  <div className={style.iconWrapper}>
+                    <Calender className={style.calender} />
+                  </div>
+                  <p className={style.checkIn} />
+                  <div className={style.iconWrapper}>
+                    <Arrow className={style.calender} />
+                  </div>
+                  <p className={style.checkOut} />
+                </li>
+              </ul>
+              <hr className={style.devider} />
+              <ul>
+                <li className={style.priceInfo}>
+                  {' '}
+                  <p className={style.roomPrice}>₩{price} x 1박</p>
+                  <p className={style.extra}> ₩{price * 1}</p>{' '}
+                </li>
+                <li className={style.priceInfo}>
+                  {' '}
+                  <div className={style.serviceWrapper}>
+                    <p className={style.roomPrice}>서비스 수수료</p>
+                    <div className={style.questionIcon}>
+                      <QuestionMark className={style.questionMark} />
+                    </div>
+                  </div>
+                  <p className={style.extra}> 아직 정보 없음</p>{' '}
+                </li>
+                <li />
+              </ul>
+              <hr className={style.devider} />
+              <div className={style.totalPrice}>
+                <p>총합계(KRW)</p>
+                <p>₩ 정보없음</p>
+              </div>
+              <hr className={style.devider} />
+            </div>
           </div>
         </div>
       </div>
