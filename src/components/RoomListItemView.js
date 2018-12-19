e731c66f788e90fd36c6de025fffdf242292220e
kimyunje <stoneaxe002@gmail.com>
2018-12-19 19:26:32 +0900

    별점 앞에 별점 평균을 숫자로 나오도록하는 기능

--- src/components/RoomListItemView.js
+++ src/components/RoomListItemView.js
@@ -26,6 +26,7 @@ export default class RoomListItemView extends Component {
         <p className={style.roomTitle}>{room.room_name}</p>
         <p className={style.roomPrice}>{room.price}원</p>
         <div className={style.starWrapper}>
+          <span>{room.rate_average}</span>
           {this.state.stars.map((star, index) =>
             star <= room.rate_average ? (
               <Star className={style.star} key={index} />
